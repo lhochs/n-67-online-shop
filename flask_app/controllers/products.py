8fb4fcf335d290d485d4e678c6b734ee3d241899
Angel Camacho Galicia <angelcamacho905@gmail.com>
2022-10-16 15:39:27 -0700

    added product form

--- flask_app/controllers/products.py
+++ flask_app/controllers/products.py
@@ -6,9 +6,9 @@ from flask_app.models.product import Product
 # This is where user can add product
 @app.route("/user/add_product")
 def add_product():
-    return render_template("add_edit_product_form.html")
+    return render_template("add_form.html")
 
 # This is where user can edit product
 @app.route("/user/edit_product/<id>")
 def edit_product(id):
-    return render_template("add_edit_product_form.html")
+    return render_template("edit_form.html")
