48988c5af8b316e1f60677c2d8d07d44cfe7b72a
Cory Duffy <duffcory@gmail.com>
2022-10-15 20:28:47 -0700

    added framework for view one-rough draft

--- flask_app/controllers/products.py
+++ flask_app/controllers/products.py
@@ -1,6 +1,6 @@
 from flask_app import app
 from flask import redirect, render_template, request, session
-from flask_app.models.product import Product
+from flask_app.models.product import Product, User
 
 
 # This is where user can add product
@@ -12,3 +12,17 @@ def add_product():
 @app.route("/user/edit_product/<id>")
 def edit_product(id):
     return render_template("add_edit_product_form.html")
+
+#route to view one product
+@app.route("/product/<id>")
+def view_product(id):
+    # if 'user_id' not in session:
+    #     return redirect('/')
+    # data = {
+    #     "id": id
+    # }
+    # user_data = {
+    #     "id": session['user_id']
+    # }
+    return render_template("view_one.html") #, product=Product.get_by_id(data), user=User.get_by_id(user_data))
+    
